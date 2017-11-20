b28a41fe7ca193e76d2d7795db51afd613976ceb
Borodin Gregory <grihabor@mail.ru>
2017-11-20 18:50:22 +0300

    add landslide cfg

--- landslide/entrypoint.sh
+++ landslide/entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-pip3 show docutils
-echo "Theme: $LANDSLIDE_THEME"
-
-
 landslide \
     --embed \
-    --theme="$LANDSLIDE_THEME" \
     --math-output \
     --verbose \
-    --destination=/build/index.html \
-    /src/index.rst
-
+    /landslide/config
+    
